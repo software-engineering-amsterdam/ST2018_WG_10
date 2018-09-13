e7044daa150f002ab2807437478b7a9e279c0e2a
loehnertz <jakob2102@gmail.com>
2018-09-13 12:38:55 +0200

    Added my implementation of exercise 2)

--- Jakob/Week2/Lab2.hs
+++ Jakob/Week2/Lab2.hs
@@ -19,3 +19,43 @@ probs n = do
 
 data Shape = NoTriangle | Equilateral
            | Isosceles  | Rectangular | Other deriving (Eq,Show)
+
+
+-- Exercises
+
+-- 1)
+
+-- 2) (20 Min.)
+
+pythagorean :: (Integer, Integer, Integer) -> Bool
+pythagorean (a,b,c) = a^2 + b^2 == c^2
+
+triangleImpossible :: (Integer, Integer, Integer) -> Bool
+triangleImpossible (a,b,c) = (a + b) < c
+
+tripleIsEqual :: (Integer, Integer, Integer) -> Bool
+tripleIsEqual (x,y,z) = (x == y) && (y == z) && (x == z)
+
+twoInTripleAreEqual :: (Integer, Integer, Integer) -> Bool
+twoInTripleAreEqual (x,y,z) = x == y || y == z || x == z
+
+triangle :: Integer -> Integer -> Integer -> Shape
+triangle a b c | triangleImpossible (a,b,c)  = NoTriangle
+               | tripleIsEqual (a,b,c)       = Equilateral
+               | pythagorean (a,b,c)         = Rectangular
+               | twoInTripleAreEqual (a,b,c) = Isosceles
+               | otherwise                   = Other
+
+{-
+Here goes the test report
+-}
+
+-- 3)
+
+-- 4)
+
+-- 5)
+
+-- 6)
+
+-- 7)
