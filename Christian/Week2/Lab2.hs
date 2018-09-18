d3aac87902ccd8b673a57d761cb06d8367e924ac
Christian <christian.jac.stuart@gmail.com>
2018-09-18 18:53:16 +0200

    rot13 stuff

--- Christian/Week2/Lab2.hs
+++ Christian/Week2/Lab2.hs
@@ -299,16 +299,20 @@ prop_rot13charLower c    = isLower c ==> ((\c' -> ord c' == (ord 'a') + (((ord c
 prop_rot13charUpper c    = isUpper c ==> ((\c' -> ord c' == (ord 'A') + (((ord c) - (ord 'A') + 13) `mod` 26)) $ rot13char c)
 prop_rot13charNonAlpha c = (not.isAlpha) c ==> (\c' -> c' == c) $ rot13char c
 
-prop_rot13Empty = rot13 "" == ""
-prop_rot13Nonempty (c:cs) = (\(c':cs') -> c' == rot13char c && cs' == rot13 cs) $ rot13 (c:cs)
+prop_rot13 [] = rot13 "" == ""
+prop_rot13 (c:cs) = (\(c':cs') -> c' == rot13char c && cs' == rot13 cs) $ rot13 (c:cs)
 
-
+checkRot13 = do
+    quickCheck prop_rot13charLower
+    quickCheck prop_rot13charUpper
+    quickCheck prop_rot13charNonAlpha
+    quickCheck prop_rot13
 
 -- exercise 7
 countrycodes = ["AF","AX","AL","DZ","AS","AD","AO","AI","AQ","AG","AR","AM","AW","AU","AT","AZ","BS","BH","BD","BB","BY","BE","BZ","BJ","BM","BT","BO","BQ","BA","BW","BV","BR","IO","BN","BG","BF","BI","KH","CM","CA","CV","KY","CF","TD","CL","CN","CX","CC","CO","KM","CG","CD","CK","CR","CI","HR","CU","CW","CY","CZ","DK","DJ","DM","DO","EC","EG","SV","GQ","ER","EE","ET","FK","FO","FJ","FI","FR","GF","PF","TF","GA","GM","GE","DE","GH","GI","GR","GL","GD","GP","GU","GT","GG","GN","GW","GY","HT","HM","VA","HN","HK","HU","IS","IN","ID","IR","IQ","IE","IM","IL","IT","JM","JP","JE","JO","KZ","KE","KI","KP","KR","KW","KG","LA","LV","LB","LS","LR","LY","LI","LT","LU","MO","MK","MG","MW","MY","MV","ML","MT","MH","MQ","MR","MU","YT","MX","FM","MD","MC","MN","ME","MS","MA","MZ","MM","NA","NR","NP","NL","NC","NZ","NI","NE","NG","NU","NF","MP","NO","OM","PK","PW","PS","PA","PG","PY","PE","PH","PN","PL","PT","PR","QA","RE","RO","RU","RW","BL","SH","KN","LC","MF","PM","VC","WS","SM","ST","SA","SN","RS","SC","SL","SG","SX","SK","SI","SB","SO","ZA","GS","SS","ES","LK","SD","SR","SJ","SZ","SE","CH","SY","TW","TJ","TZ","TH","TL","TG","TK","TO","TT","TN","TR","TM","TC","TV","UG","UA","AE","GB","US","UM","UY","UZ","VU","VE","VN","VG","VI","WF","EH","YE","ZM","ZW"]
 
 -- Not necessarily true.
-bban country chars = True
+bban country chars = country `elem` countrycodes
 
 
 iban (coa:cob:cha:chb:bbanchars) = bban [coa,cob] bbanchars && (mod97 $ bbanchars ++ [coa, cob, cha, chb])
