b39a99f5efb785aa0d4c3658932b68db00dd451a
loehnertz <jakob2102@gmail.com>
2018-09-26 16:08:12 +0200

    Initialized Lab4

---
+++ Jakob/Week4/Lab4.hs
@@ -0,0 +1,46 @@
+module Lab4 where
+
+import           Data.List
+import           SetOrd
+import           System.Random
+import           Test.QuickCheck
+
+
+-- Exercises
+
+-- 1)
+
+
+
+
+-- 2)
+
+
+
+
+-- 3)
+
+
+
+
+-- 4)
+
+
+
+
+-- 5)
+
+
+
+
+-- 6)
+
+
+
+
+-- 7)
+
+
+
+
+-- 8)
