12ec0866619a04f4c93de3eb2ce3b158b61fc619
loehnertz <jakob2102@gmail.com>
2018-09-26 16:34:50 +0200

    Completed exercise 2)

--- Jakob/Week4/Lab4.hs
+++ Jakob/Week4/Lab4.hs
@@ -13,9 +13,21 @@ import           Test.QuickCheck
 
 
 
--- 2)
+-- 2) (15 Min.)
 
+-- From scratch
+genRandIntBetween1And10 :: IO Int
+genRandIntBetween1And10 = randomRIO (1 :: Int, 10 :: Int)
 
+setGenScratch :: IO (Set Int)
+setGenScratch = do
+  length <- genRandIntBetween1And10
+  list <- sequence (replicate length genRandIntBetween1And10)
+  return (list2set list)
+
+-- With QuickCheck
+instance (Arbitrary a, Ord a) => Arbitrary (Set a) where
+  arbitrary = fmap list2set arbitrary
 
 
 -- 3)
