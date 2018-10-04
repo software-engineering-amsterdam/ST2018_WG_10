bb8d458d8bbddfd40f0e2c80fa5abd94aa191352
loehnertz <jakob2102@gmail.com>
2018-10-04 11:35:17 +0200

    Added the exercises of this week

---
+++ Jakob/Week5/Lab5.hs
@@ -0,0 +1,30 @@
+module Lab5 where
+
+import           Data.List
+import           Lecture5
+import           System.Random
+
+
+-- Exercises
+
+-- 1)
+
+
+
+
+-- 2)
+
+
+
+
+-- 3)
+
+
+
+
+-- 4)
+
+
+
+
+-- 5)
