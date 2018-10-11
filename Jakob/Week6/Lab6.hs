aaad45e765f8da17cafda37e5d654635e2eb4342
loehnertz <jakob2102@gmail.com>
2018-10-11 11:54:18 +0200

    Add scaffold files for week 6

---
+++ Jakob/Week6/Lab6.hs
@@ -0,0 +1,35 @@
+module Lab6 where
+
+import           Data.List
+import           Lecture6
+import           System.Random
+
+
+-- Exercises
+
+-- 1)
+
+
+
+
+-- 2)
+
+
+
+
+-- 3)
+
+
+
+
+-- 4)
+
+
+
+
+-- 5)
+
+
+
+
+-- 6)
