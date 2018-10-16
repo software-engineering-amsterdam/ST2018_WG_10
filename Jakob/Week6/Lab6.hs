98ce5f71064b532c68337160a208168dca0ab0a5
loehnertz <jakob2102@gmail.com>
2018-10-16 16:44:31 +0200

    Finished exercises 1 and 3

--- Jakob/Week6/Lab6.hs
+++ Jakob/Week6/Lab6.hs
@@ -3,13 +3,21 @@ module Lab6 where
 import           Data.List
 import           Lecture6
 import           System.Random
+import           Test.QuickCheck
 
 
 -- Exercises
 
 -- 1)
 
+exM' :: Integer -> Integer -> Integer -> Integer
+exM' b 1 m = 1
+exM' b x m = ((b^2) `mod` m) * (exM' b (div x 2) m)
 
+prop_ExpEquiv :: (NonNegative Integer) -> (NonNegative Integer) -> (Positive Integer) -> Bool
+prop_ExpEquiv (NonNegative x) (NonNegative y) (Positive z) = expM x y z == exM' x y z
+
+check_ExpEquiv = quickCheck prop_ExpEquiv
 
 
 -- 2)
@@ -19,17 +27,5 @@ import           System.Random
 
 -- 3)
 
-
-
-
--- 4)
-
-
-
-
--- 5)
-
-
-
-
--- 6)
+composites :: [Integer]
+composites = [x | x <- [2..], not . prime $ x]
